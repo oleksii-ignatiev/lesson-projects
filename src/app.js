870cbb29b717e87f7b1aac1ff416dd88877f8521
Andrii Mulyk <amulyk@lectrum.io>
2020-01-28 20:25:28 +0200

    add files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { Empty } from './source/empty';
-import { List } from './source/list';
+// import { List } from './source/list';
+import { TaskForm } from './source/TaskForm';
 
 export const App = () => {
   return (
     <>
       {/*<Empty />*/}
-      <List />
+      {/*<List />*/}
+      <TaskForm />
     </>
   )
 };
