b91b7e478ca9d9de332280443aa812ab115ab681
Oleksii Ignatiev <o.ignatiev@knubisoft.com>
2020-03-23 15:58:29 +0200

    anotheк approach

--- src/lesson-2-3/components/accordeon/index.js
+++ src/lesson-2-3/components/accordeon/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import Styles from './styles.scss';
-import { Question } from '../question';
+import React, { useState } from 'react';
+import cx from 'classnames';
+import Styles from './styles.module.scss';
+//import { Question } from '../question';
 
 export const Accordeon = (props) => {
-	const accordeon = props.source || [];
+    const accordion = props.source || [];
+    const [active, setActive] = useState(0);
+    const toggle = (index) => setActive(index);
     
     return (
-        <section className="accordion">
+        <section className = {Styles.accordion} >
             <h1>Accordion</h1>
-			{accordeon.map( 
-                (item, index) => <Question key = {index} question = {item} /> 
-			)}
+			{accordion.map( (item, index) => {
+                
+                const accordionCX = cx({
+                    [ Styles['accordion-item'] ]: true, 
+                    [ Styles['selected'] ]: (active === index) ? active : index 
+                })
+                   
+                return (
+                    //<Question key = { index }  index = { index } question = {item} styles = {Styles} onClick = { () => toggle(index) }/>
+                    <div key = { index } className = { accordionCX } onClick = { () => toggle(index) } >
+                        <div className={Styles['accordion-question']}>
+                            <span>{ item.question }</span>
+                        </div>
+                        <p className={Styles['accordion-answer']}>
+                            { item.answer }
+                        </p>
+                    </div>   
+                )
+            })}
 		</section>
     )
 };
--- src/lesson-2-3/components/question/index.js
+++ src/lesson-2-3/components/question/index.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
+import cx from 'classnames';
 
 export const Question = (props) => {
-	const question = props.question || {};
-    const [isSelected, setSelected] = useState(false);
+    const question = props.question;
+    const Styles = props.styles;
+    const index = props.index;
     
-    const _toggle = () => {
-        setSelected( (isSelected) => !isSelected );
-    }
-
+    const [active, setActive] = useState(0);
+    const toggle =(index)=>setActive(index);
+    
+    const accordionCX = cx({
+        [ Styles['accordion-item'] ]: true, 
+        [ Styles['selected'] ]: active 
+    });
+    
     return (
-        <div className = { `accordion-item ${isSelected ? "selected" : "" }` } onClick = { _toggle } >
-            <div className='accordion-question'>
+        <div className = { accordionCX } onClick = { () => toggle(index) } >
+            <div className={Styles['accordion-question']}>
                 <span>{ question.question }</span>
             </div>
-            <p className='accordion-answer'>
+            <p className={Styles['accordion-answer']}>
                 { question.answer }
             </p>
         </div>
