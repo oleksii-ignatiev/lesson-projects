c85d2bee56bd37945c0dda000562cc4d26901591
Oleksii Ignatiev <o.ignatiev@knubisoft.com>
2020-03-23 16:46:43 +0200

    added Title with updated leddon-2-3

--- src/lesson-2-4/components/accordeon/index.js
+++ src/lesson-2-4/components/accordeon/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import Styles from './styles.scss';
-import { Question } from '../question';
+import React, { useState } from 'react';
+import cx from 'classnames';
+import Styles from './styles.module.scss';
 
 export const Accordeon = (props) => {
-	const accordeon = props.source || [];
-	const accordeonTitle = props.title || 'Accordion';
+    const accordeonTitle = props.title || 'Accordion';
+    const accordion = props.source || [];
+    const [active, setActive] = useState(0);
+    const toggle = (index) => setActive(index);
     
     return (
-        <section className="accordion">
+        <section className = {Styles.accordion} >
             <h1>{ accordeonTitle }</h1>
-			{accordeon.map( 
-                (item, index) => <Question key = {index} question = {item} /> 
-			)}
+			{accordion.map( (item, index) => {
+                
+                const accordionCX = cx({
+                    [ Styles['accordion-item'] ]: true, 
+                    [ Styles['selected'] ]: (active === index) ? true : false
+                })
+                   
+                return (
+                    <div key = { index } className = { accordionCX } onClick = { () => toggle(index) } >
+                        <div className={Styles['accordion-question']}>
+                            <span>{ item.question }</span>
+                        </div>
+                        <p className={Styles['accordion-answer']}>
+                            { item.answer }
+                        </p>
+                    </div>   
+                )
+            })}
 		</section>
     )
 };
