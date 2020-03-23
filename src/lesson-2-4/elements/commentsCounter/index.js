3e9d5f3b19f14f682abb5bba1ca951e05fc5958c
Oleksii Ignatiev <o.ignatiev@knubisoft.com>
2020-03-23 18:25:20 +0200

    folder lesson-2-4 deleted

--- src/lesson-2-4/elements/commentsCounter/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Styles from './styles.module.scss';
-
-export const CommentsCounter = (props) => {
-	const commentCount = props.commentCounter || '0';
-  return (
-    <span className={ Styles.commentsCounter }>{ commentCount }</span>
-  )
-};
