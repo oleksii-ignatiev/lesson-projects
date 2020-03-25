6048c9ca6cc42457ecd159ce7c4e96846a4f3ec3
Oleksii Ignatiev <o.ignatiev@knubisoft.com>
2020-03-26 01:15:49 +0200

    loader added

--- src/lesson-3-1/components/news/index.js
+++ src/lesson-3-1/components/news/index.js
@@ -1,35 +1,20 @@
 import React from 'react';
 
+// Components
 import { Article } from '../article';
 
-const db = [
-    {
-        id: 'H2K4J',
-        isConfirmed: true,
-        isSelected: true,
-        title: 'На марсе нашли жизнь',
-        description: 'Во время очередной миссии на марс, жителям земли удалось встретить существ очень похожих на человека'
-    },
-    {
-        id: 'Z8L1S',
-        title: 'Apple начала раздавать iPhone бессплатно',
-        description: 'Вчера вечером кампания Apple анонсировала что теперь любой человек может получить iPhone бессплатно'
-    },
-    {
-        id: 'S1N1T',
-        hidden: true,
-        title: 'Илон Маск научился добывать энергию из воздуха',
-        description: 'Успешный предприниматель и инженер Илон Маск заявил что научился добывать энергию из воздуха'
-    },
-];
+// Hooks
+import { useApiLoader } from './useApiLoader';
 
 export const News = () => {
-    const articleJSX = db.map(({id, ...props}) => (<Article key = { id } { ...props } />));
-
+    const { db, isFetching } = useApiLoader();
+    const articleJSX = db.map(({objectId, ...props}) => (<Article key = { objectId } { ...props } />));
+    const loadingDataJSX = <p>Загрузка данных</p>;
     return (
         <section>
             <h3>News</h3>
-            { articleJSX }
+            { (isFetching) ? loadingDataJSX : articleJSX }
+            { (isFetching) ? console.log('Загрузка данных') : console.log('Загрузка не идет') }
         </section>
     );
 };
--- src/lesson-3-1/components/news/useApiLoader.js
+++ src/lesson-3-1/components/news/useApiLoader.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
-import { api } from '../../api.js';
+// Core
+import {useEffect, useState} from 'react';
 
-export const useArticle = (hidden) => {
-    const [ isArticleVisible, setVisible ] = useState(!hidden);
+// Api
+import { api } from '../../api';
 
+export const useApiLoader = () => {
+    console.log('hook render');
+    
+    const [db, setDb] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+    
+    useEffect(() => {
+        (async () => {
+            const response = await api.news.fetch();
+            const data = await response.json();
+            setIsFetching(true);
+            setDb(data);
+        })();
+    }, []);
+
+    useEffect(() => {
+        setIsFetching(false);
+    }, [db]);
+    
     return {
-        setVisible,
-        isArticleVisible,
+        db,
+        isFetching
     }
 }
